1296a235578c5e0d4548e67e092112284b3914ba
Slyke <steven.lawler777@gmail.com>
2021-05-23 17:23:29 -0700

    Switch shell type

--- .internal/pycli/deps/host_exec.py
+++ .internal/pycli/deps/host_exec.py
@@ -16,7 +16,7 @@ def getCommandString(command, iotstackPwd=iotstackPwd, hostUser=hostUser, hostAd
   )
 
 def execSilent(command):
-  execRes = subprocess.check_output(getCommandString(command), stderr=subprocess.PIPE, shell=False)
+  execRes = subprocess.check_output(getCommandString(command), stderr=subprocess.PIPE, shell=True)
   return execRes.decode('ascii').strip()
 
 def execInteractive(command):
