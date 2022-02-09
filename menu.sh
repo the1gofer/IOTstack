a7a90e9899674be0f190b9b1d8a4482ca4547727
Phill Kelley <34226495+Paraphraser@users.noreply.github.com>
2022-02-09 14:37:59 +1100

    20211116 hassio - old-menu branch - PR 2 of 2
    This is a response to [Issue 491](https://github.com/SensorsIot/IOTstack/issues/491).
    
    This PR removes the "Install Home Assistant (Requires Docker)" functionality for installing Supervised Home Assistant from the IOTstack menu. The command invokes:
    
    ```
    curl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh | sudo bash -s
    ```
    
    but the `installer.sh ` script no longer exists (404 error), so the IOTstack menu command fails.
    
    From a user perspective, there are four options:
    
    1. Run the standalone `home_assistant` container selectable from the IOTstack menu.
    2. Build a Raspberry Pi with [PiBuilder](https://github.com/Paraphraser/PiBuilder) so that Supervised Home Assistant and IOTstack can coexist on the same hardware.
    3. Replicate what PiBuilder does to install Supervised Home Assistant alongside IOTstack.
    4. Dedicate a Raspberry Pi to the task of running Supervised Home Assistant as an appliance. This appears to be the direction favoured by the folks at Home Assistant.
    
    This PR reduces old-menu IOTstack Home Assistant documentation to a stub pointing to the Home Assistant documentation on master branch.
    
    Signed-off-by: Phill Kelley <34226495+Paraphraser@users.noreply.github.com>

--- menu.sh
+++ menu.sh
@@ -416,7 +416,6 @@ mainmenu_selection=$(whiptail --title "Main Menu" --menu --notags \
 	"" 20 78 12 -- \
 	"install" "Install Docker" \
 	"build" "Build Stack" \
-	"hassio" "Install Home Assistant (Requires Docker)" \
 	"native" "Native Installs" \
 	"commands" "Docker commands" \
 	"backup" "Backup options" \
@@ -647,33 +646,6 @@ case $mainmenu_selection in
 	esac
 	;;
 
-"hassio")
-	echo "install requirements for hass.io"
-	sudo apt install -y bash jq curl avahi-daemon dbus
-	hassio_machine=$(whiptail --title "Machine type" --menu \
-		"Please select you device type" 20 78 12 -- \
-		"raspberrypi4" " " \
-		"raspberrypi3" " " \
-		"raspberrypi2" " " \
-		"raspberrypi4-64" " " \
-		"raspberrypi3-64" " " \
-		"qemux86" " " \
-		"qemux86-64" " " \
-		"qemuarm" " " \
-		"qemuarm-64" " " \
-		"orangepi-prime" " " \
-		"odroid-xu" " " \
-		"odroid-c2" " " \
-		"intel-nuc" " " \
-		"tinker" " " \
-		3>&1 1>&2 2>&3)
-	if [ -n "$hassio_machine" ]; then
-		curl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh | sudo bash -s -- -m $hassio_machine
-	else
-		echo "no selection"
-		exit
-	fi
-	;;
 "update")
 	echo "Pulling latest project file from Github.com ---------------------------------------------"
 	git pull origin $CURRENT_BRANCH
